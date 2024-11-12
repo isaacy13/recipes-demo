8202e832b501544897c811260559c433ad6c166b
Isaac Yeang <isaacy13@icloud.com>
2024-11-11 22:22:13 -0600

    added comment

--- recipes-demo/Common/Constants.swift
+++ recipes-demo/Common/Constants.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+// normally, Info.plist would not exist in repository and be built in pipeline
 struct Constants {
     static let RECIPE_HOST_URL = Bundle.main.object(forInfoDictionaryKey: "RECIPE_HOST_URL") as! String
 }
