c71c811d6b3dcc8e5e9a1acabb01901763e34c70
Isaac Yeang <isaacy13@icloud.com>
2024-11-16 14:02:46 -0600

    unit tests

--- recipes-demoTests/HomeViewModelTests.swift
+++ recipes-demoTests/HomeViewModelTests.swift
@@ -2,37 +2,53 @@ import XCTest
 @testable import DEV_recipes_demo
 
 final class HomeViewModelTests: XCTestCase {
-    private var viewModel: HomeViewModel!
+    private var viewModel = HomeViewModel()
     
-    override func setUp() {
-        super.setUp()
-        viewModel = HomeViewModel()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-        viewModel = nil
-    }
-    
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    // use @MainActor to ensure main thread updates execute before checking state
+    @MainActor
     func test_ValidRecipeLoads() async throws {
         let recipes = viewModel.recipes
         await viewModel.loadRecipes(type: .all)
         XCTAssertGreaterThan(viewModel.recipes.count, recipes.count)
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        XCTAssertEqual(viewModel.isLoading, false)
+        XCTAssertNil(viewModel.alertError)
+        
+        addTeardownBlock {
+            self.viewModel.recipes = []
         }
     }
-
+    
+    @MainActor
+    func test_MalformedRecipeLoads() async throws {
+        let recipes = viewModel.recipes
+        await viewModel.loadRecipes(type: .malformed)
+        XCTAssertEqual(viewModel.recipes.count, recipes.count)
+        XCTAssertEqual(viewModel.isLoading, false)
+        XCTAssertNotNil(viewModel.alertError)
+        
+        let apiError = viewModel.alertError as? ApiError
+        XCTAssertNotNil(apiError)
+        
+        let isUnexpectedApiError: Bool = {
+            if case .Unexpected(description: _) = apiError { return true }
+            else { return false }
+        }()
+        XCTAssert(isUnexpectedApiError)
+        
+        addTeardownBlock {
+            self.viewModel.recipes = []
+        }
+    }
+    
+    @MainActor
+    func test_EmptyRecipeLoads() async throws {
+        await viewModel.loadRecipes(type: .empty)
+        XCTAssertEqual(viewModel.recipes.count, 0)
+        XCTAssertEqual(viewModel.isLoading, false)
+        XCTAssertNil(viewModel.alertError)
+        
+        addTeardownBlock {
+            self.viewModel.recipes = []
+        }
+    }
 }
